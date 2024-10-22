9629ac5827ab1a17c1daf1b59466151cb14c8732
nzyoka10 <mnzyokaeric@gmail.com>
2024-10-22 10:12:06 +0300

    lesson 1

---
+++ 0x08-kotlin/lesson1.kt
@@ -0,0 +1,10 @@
+// first program
+fun main(){
+    print("Hello Kotlin")
+
+    // Data types
+    val mynumber = 5 // int
+    val weight = 59.8 // double
+    val name = "Nzyoka" // string
+    val letter = 'K' // char
+}
